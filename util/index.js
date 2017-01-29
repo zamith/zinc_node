c4f00fe77a1659d8eb5f3878cfa2ec907a7e1313
dany92 <dany@bc.edu>
2017-01-29 15:42:23 -0500

    indention corrected

--- util/index.js
+++ util/index.js
@@ -1,16 +1,16 @@
  /*RECURSIVELY SEARCH FOR SPEAKER TAG IN THE XML OBJECT 
  AND RETURN DICTIONARY*/
 
-var FindByTag = function(xml, tagName, dictionary){
+var FindByTag = function(xml, tagName, dictionary) {
 	//IF XML IS NOT AN ARRAY OR OBJECT THE TAG CANNOT BE FOUND
 	if(!Array.isArray(xml) && !(typeof xml === 'object')) return;
 
 	let dictionaryHandler = (k) => {
-	let upperKey = k.toUpperCase();
-	if(!(upperKey === 'ALL')){
-	  	(dictionary.hasOwnProperty(upperKey))? 
-	  	dictionary[upperKey]++ : dictionary[upperKey] = 1;
-	}
+		let upperKey = k.toUpperCase();
+		if(!(upperKey === 'ALL')){
+		  	(dictionary.hasOwnProperty(upperKey))? 
+		  	dictionary[upperKey]++ : dictionary[upperKey] = 1;
+		}
 	}
 	for(key in xml){
 		if(key === tagName){
