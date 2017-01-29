c0c2f42a158cacbc6f93bb07ef45d4c7be86b84e
dany92 <dany@bc.edu>
2017-01-29 15:37:50 -0500

    recursion problem fixed. needed type checker base case

--- util/index.js
+++ util/index.js
@@ -2,22 +2,25 @@
  AND RETURN DICTIONARY*/
 
 var FindByTag = function(xml, tagName, dictionary){
-  let dictionaryHandler = (k) => {
-    let upperKey = k.toUpperCase();
-    if(!(upperKey === 'ALL')){
-      (dictionary.hasOwnProperty(upperKey))? 
-      dictionary[upperKey]++ : dictionary[upperKey] = 1;
-    }
-  }
-  for(key in xml){
-    if(key === tagName){
-      if(!Array.isArray(xml[key])) xml[key] = [xml[key]];
-      dictionaryHandler.apply(this, xml[key])
-    } else {
-      FindByTag(xml[key], tagName, dictionary);
-    }
-  }
-  return dictionary;
+	//IF XML IS NOT AN ARRAY OR OBJECT THE TAG CANNOT BE FOUND
+	if(!Array.isArray(xml) && !(typeof xml === 'object')) return;
+
+	let dictionaryHandler = (k) => {
+	let upperKey = k.toUpperCase();
+	if(!(upperKey === 'ALL')){
+	  	(dictionary.hasOwnProperty(upperKey))? 
+	  	dictionary[upperKey]++ : dictionary[upperKey] = 1;
+	}
+	}
+	for(key in xml){
+		if(key === tagName){
+		  	if(!Array.isArray(xml[key])) xml[key] = [xml[key]];
+		  	dictionaryHandler.apply(this, xml[key])
+		} else {
+		  	FindByTag(xml[key], tagName, dictionary);
+		}
+	}
+	return dictionary;
 }
 
 module.exports = { FindByTag };
