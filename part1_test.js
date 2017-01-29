311a646c317fc1d0c9db7827c2470106ea4cb00f
dany92 <dany@bc.edu>
2017-01-29 18:47:21 -0500

    all tests passing

---
+++ part1_test.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var xml2js = require('xml2js');
+var { FindByTag } = require('./util');
+
+//PART 1 BUT NO DOWNLOAD
+function getAnalysis(url){
+    let dictionary = {};
+    //REQUEST XML DATA
+    var req = http.get(url, function(res) {
+        var xmlString = '';
+
+        res.on('data', function(chunk) {
+            xmlString += chunk;
+        });
+
+        res.on('end', function() {
+            //PARSER FOR XML STRING
+            var parser = new xml2js.Parser();
+            parser.parseString(xmlString, (err, result) => {
+                if(err){
+                    return promise;
+                } else{
+                    dictionary = FindByTag(result, "SPEAKER", {});
+                }
+            });
+        });
+    });
+    //HANDLE REQUEST ERROR
+    req.on('error', function(err) {
+        return promise;
+    });
+    //RETURN PROMISE TO TEST FILE
+    var promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if(Object.keys(dictionary).length > 0){
+                resolve(dictionary);
+            }else{
+                reject({message: "Time out. Problem fetching the data"});
+            }
+        }, 1000);
+    });
+    return promise;
+  
+}
+
+module.exports = getAnalysis;
--- tests/index.js
+++ tests/index.js
@@ -3,10 +3,75 @@ let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
 let expect = chai.expect;
+let getAnalysis = require('../part1_test.js');
 
-describe('testing', function() {
-	it('should pass basic math', function(done){
-		expect(1+1).to.equal(2);
-		done()
-	})
+const url = 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml';
+chai.use(chaiHttp);
+
+describe('Fetch Macbeth xml without downloading', function() {
+	var dictionary;
+
+	before(function(done){
+		getAnalysis(url)
+		.then((result) => {
+			dictionary = result;
+			done();
+		})
+	});
+
+	it('should return an object with character as the key and number of lines as the value', function(done) {
+			expect(dictionary).to.be.an('object');
+			done();
+		}
+	);
+
+	it('should contain Macbeth as key', function(done) {
+			expect(dictionary).to.include.keys('MACBETH');
+			done();
+	});
+
+	it('should contain number as value', function(done) {
+			expect(dictionary['MACBETH']).to.be.a('number');
+			done();
+	});
+
 });
+
+
+describe('Fetch non-existing url using http module', function() {
+	it('should return ENOTFOUND error when given non-http invalid link and res is undefined', function(done){
+		let request = chai.request("dafdsfsdf")
+		.get('/')
+		.end(function(err, res){
+			expect(err.code).to.eql("ENOTFOUND");
+			expect(res).to.be.an('undefined');
+			done()
+		})
+	})
+
+	it('given non-existing link', function(done){
+		getAnalysis("http://www.ibiblio.org/xml/examples/shakespeare/mac.xml")
+		.then((result) => {
+			done();
+		})
+		.catch(err => {
+			expect(err.message).to.equal("Time out. Problem fetching the data");
+			done()
+		})
+	})
+
+})
+
+
+//ROUTER TEST
+// describe('ANALYSIS', function() {
+// 	it('should fetch object with characters /analyze POST', function(done) {
+// 		chai.request(server)
+// 		.post('/analyze')
+// 		.send({$url: "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"})
+// 		.end(function(err, res){
+// 			res.should.have.status(200);
+// 			done();
+// 		});
+// 	});
+// });
